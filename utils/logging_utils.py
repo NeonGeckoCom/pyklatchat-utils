b456cf201406fcc87ee40936fa6104b53f02b4fb
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2025-02-25 10:57:02 -0800

    Packaging, Tests, and Automation (#1)
    * Implement latest code from pyklatchat
    Add Python packaging and GHA
    Outline minimal unit test coverage
    
    * Update requirements to address test failures
    
    * Move sshtunnel dependency to `sql` as it is only used for migration from sql
    
    * Refactor LOG imports
    Add `configuration` module
    
    * Add ovos-utils dependency
    
    * Fix import
    Move sftp dependency to extras for license compat.
    
    * Fix imports in `configuration` module
    
    * Update type annotations for Python 3.7/3.8 compat.
    
    * Update Python tested versions
    
    * Whitelist tqdm with MIT license
    
    * Implement tests for `common` submodule with more explicit error handling
    
    * Simplify configuration module
    Add docstrings and type annotations
    
    * Add unit test coverage for configuration module
    Fix error in config validation
    
    * Include default templates
    Update `template_utils` default template path handling
    Add test coverage for `template_utils` and `http_utils` modules
    
    * Add HTTP Exceptions and update import in `database_utils` submodule
    Failure noted in https://github.com/NeonGeckoCom/pyklatchat/actions/runs/12564129275/job/35026866035?pr=126
    
    * Refactor `pyklatchat_utils` to `klatchat_utils` for consistent naming with observer module
    
    * Fix missed `pyklatchat` -> `klatchat` refactor
    
    * Update license notices to 2025
    
    * Integrate changes from upstream Klatchat
    https://github.com/NeonGeckoCom/pyklatchat/pull/107
    https://github.com/NeonGeckoCom/pyklatchat/pull/148
    https://github.com/NeonGeckoCom/pyklatchat/pull/128
    
    * Update imports from rebased utils changes

--- utils/logging_utils.py
+++
@@ -1,7 +0,0 @@
-import importlib
-import logging
-
-combo_lock_logger = logging.getLogger("combo_lock")
-combo_lock_logger.disabled = True
-
-LOG = getattr(importlib.import_module('ovos_utils'), 'LOG')
